e0e49520ed84d9c4a60b205028bd29d46fe926da
Kabir Deula <kabirdeula167@gmail.com>
2025-01-12 15:46:40 +0545

    Implement home screen layout with a ListView displaying placeholder text

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -5,6 +5,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Text('Charmander');
+          },
+        ),
+      ),
+    );
   }
 }
